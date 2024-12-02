1084d941e4950ee55da6edd0c3d96cd4c052a8e7
skwyarspro15 <potatobg648@gmail.com>
2024-12-02 14:35:08 +0800

    Added boot screen

--- pkgs/system/bootloader.js
+++ pkgs/system/bootloader.js
@@ -7,7 +7,32 @@ export default {
     Root.Lib.setOnEnd((_) =>
       console.log("how do you even kill the boodloader")
     );
-    Root.Lib.launch("ui:taskbar");
+    let bootScreen = new Html("div")
+      .styleJs({
+        backgroundColor: "black",
+        zIndex: 100000,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        color: "white",
+      })
+      .appendMany(
+        new Html("img").attr({ src: "assets/Midlight.svg" }).styleJs({
+          width: "100px",
+          height: "100px",
+        }),
+        new Html("br"),
+        new Html("h2").text("Please wait...")
+      )
+      .appendTo(wrapper);
+    await Root.Lib.launch("ui:taskbar");
+    bootScreen.cleanup();
     setTimeout(() => {
       Root.Lib.launch("apps:helloWorld");
     }, 500);
